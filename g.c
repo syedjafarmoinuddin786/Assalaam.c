8f6fce2b4b94c923cbdb55383412d5d04330f752
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2023-12-11 19:41:56 +0530

    playing with characters

---
+++ g.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char c,s[100],sen[100];
+    scanf("%c\t%s\n",&c,s);
+    gets(sen);
+    printf("%c\n",c);
+    printf("%s\n",s);
+    printf("%s",sen);
+    return 0;
+}
+
